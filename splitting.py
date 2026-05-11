30aef814875dd82e73135fe5b9a04d39a5f6b37f
AlexeiYaganov <35544842+Alexeiyaganov@users.noreply.github.com>
2026-05-11 05:52:57 +0000

    edit imports in splitting

--- splitting.py
+++ splitting.py
@@ -18,7 +18,7 @@ from __future__ import annotations
 
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedKFold, train_test_spli
 
 
 def split_data(
